090224b880cd62001fe315de2b7b2993a622b373
Lautaro Addamo <105892155+LautaroAddamo@users.noreply.github.com>
2022-08-19 16:20:04 -0300

    Update Main.java

--- Ejercicio1Tienda/src/Tienda/Main.java
+++ Ejercicio1Tienda/src/Tienda/Main.java
@@ -5,17 +5,7 @@ import Tienda.Servicios.ProductoServicios;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        ProductoServicios ps = new ProductoServicios();
-
-        ps.listarProductos();
-
-        int opc = 1;
-        switch (opc) {
-            case 1:
-                break;
-            case 2:
-                break;
-        }
+       
     }
 
 }
