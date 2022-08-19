b5372e1bbf2684ce33aa9a5b82225cd80de219c7
Lautaro Addamo <105892155+LautaroAddamo@users.noreply.github.com>
2022-08-19 16:31:58 -0300

    Update Main.java

--- Ejercicio1Tienda/src/Tienda/Main.java
+++ Ejercicio1Tienda/src/Tienda/Main.java
@@ -5,13 +5,11 @@ import Tienda.Orquestador.Orquestador;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
-        ;
+
+        
         Orquestador.crearOrquestador();
 
-=======
-       
->>>>>>> 090224b880cd62001fe315de2b7b2993a622b373
+
     }
 
 }
